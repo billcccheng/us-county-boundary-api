4f133156c6aab566a4db8a04c60b2e18d79231a4
Bill <iamkg05@gmail.com>
2016-12-09 17:01:26 -0600

    debugging XSS

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
 var express = require("express");
 var app = express()
 var fs = require("fs");
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+app.configure(function () {
+  app.use(express.bodyParser());
+  app.use(allowCrossDomain);
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
 
 app.get('/api',function(req, res){
 	fs.readFile(__dirname + '/states/' + req.query.state + '.json', function(err, data){
